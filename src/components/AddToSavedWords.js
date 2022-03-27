5cd64fb76e9f9d78a27d5b9cf15f2002c164ecbc
Mullins, Benjamin <benm@umich.edu>
2022-03-27 16:41:06 -0400

    tips

--- src/components/AddToSavedWords.js
+++ src/components/AddToSavedWords.js
@@ -1,6 +1,11 @@
+// App.js is already creating savedWordsArray as state having this here could potentially
+// cause all sorts of failures.
 const savedWordsArray = [];
 
 function AddToSavedWords(word) {
+    // There's a variable called savedWords used several times.
+    // It doesn't exist, so you'll crash as soon as AddToSavedWords
+    // is callewd.
     if (savedWordsArray.length === 0 ) {
         savedWords.innerHTML = `(none)`;
     }
@@ -11,6 +16,9 @@ function AddToSavedWords(word) {
     else {
         savedWords.append(", " + word);
     }
+    // App.js has a setSavedWordsArray for updating the savedWordsArray state.
+    // This savedWordsArray.push is doing nothing at best, but potentially
+    // crashing the app.
     savedWordsArray.push(word);
 }
 
--- src/components/SavedWords.js
+++ src/components/SavedWords.js
@@ -2,15 +2,24 @@
 
 
 const SavedWords = (props) => {
+    // All you need here is savedSetWordsArray. You're not
+    // changing its value here, just displaying it.
     const {savedWordsArray, setSavedWordsArray, word} = props;
     // console.log(props);
 
+    // You don't need a button handler here, there's no button! 🙂
+    // This is just displaying something that was set elsewhere.
     const buttonHandler = (event) => {
-        AddToSavedWords = () => {
-            if (savedWordsArray.length === 0 ) {
-                console.log(savedWordsArray.length)
-                return 'none';
-            }
+        // This seems to be declaring a new function variable
+        // without adding a const or let, so it's going to fail
+        // I commented it because it was stopping the app from
+        // compiling.
+        // AddToSavedWords = () => {
+        //     if (savedWordsArray.length === 0 ) {
+        //         console.log(savedWordsArray.length)
+        //         return 'none';
+        //     }
+
         // else if (savedWordsArray.length === 1 ) {
         //     savedWordsArray(null);
         //     setSavedWordsArray.append(word);
@@ -21,11 +30,18 @@ const SavedWords = (props) => {
         // setSavedWordsArray.push(word);
     }
 
-    AddToSavedWords();
+    // Commented away the line below because it's calling a
+    // function that doesn't exist.
+    //AddToSavedWords();
+
+    // All you really need to do *here* is display an array of saved words
+    // If I was asked to do that back when I was paid to write JS without
+    // having been taught it, I'd search for something like
+    // "js array to comma separated string"
 
     return (
         // do something here
         <div className="col">Saved words: </div>
-)}}
-
+)}
+// had to remove an extra curly brace 👆 so it would work.
 export default SavedWords;
